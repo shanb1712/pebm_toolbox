d87b937075a92b19d307217843ca7e56c6ff03d6
sheina <sheina@campus.technion.ac.il>
2021-06-13 17:23:07 +0300

    v3_corected

---
+++ build/lib/pebm_pkg/FiducialPoints.py
@@ -0,0 +1,18 @@
+
+class FiducialPoints:
+
+    def __init__(self, signal, fs, peaks= None):
+        self.signal = signal
+        self.fs = fs
+        if peaks is None:
+            pre = Preprocessing(signal, fs)
+        self.peaks= peaks
+
+    def compute_fiducial(self):
+        signal = self.signal
+        fs = self.fs
+        peaks = self.peaks
+        #exe from snir
+        position = 0
+        return position
+
